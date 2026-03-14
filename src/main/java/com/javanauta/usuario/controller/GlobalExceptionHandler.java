e68604151fd4112b9f9fb229197a2d124cb3b4d3
Aureo <aureoandradeprofissional@gmail.com>
2026-03-14 14:46:34 -0300

    adicionado consulta na api externa do via cep

---
+++ src/main/java/com/javanauta/usuario/controller/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.javanauta.usuario.controller;
+
+import com.javanauta.usuario.infrastructure.exceptions.ConflictException;
+import com.javanauta.usuario.infrastructure.exceptions.ResourceNotFoundExeception;
+import com.javanauta.usuario.infrastructure.exceptions.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler (ResourceNotFoundExeception.class)
+    public ResponseEntity <String> handleResourceNotFoundException(ResourceNotFoundExeception ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler (ConflictException.class)
+    public ResponseEntity <String> handleConflictException (ConflictException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler (UnauthorizedException.class)
+    public ResponseEntity <String> handleUnauthorizedException (UnauthorizedException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+    @ExceptionHandler (IllegalArgumentException.class)
+    public ResponseEntity <String>  handleIllegalArgumentException(IllegalArgumentException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+}
---
+++ src/main/java/com/javanauta/usuario/infrastructure/exceptions/IllegalArgumentException.java
@@ -0,0 +1,10 @@
+package com.javanauta.usuario.infrastructure.exceptions;
+
+public class IllegalArgumentException extends RuntimeException {
+    public IllegalArgumentException(String mensagem) {
+        super(mensagem);
+    }
+    public IllegalArgumentException (String mensagem, Throwable throwable){
+        super(mensagem,throwable);
+    }
+}
